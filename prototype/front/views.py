1d3bedcd45ee5740774bdeafb604f2e7609f833c
bigfootledur <antoine.omond@ntymail.com>
2021-01-27 13:05:33 +0100

    miss

--- prototype/front/views.py
+++ prototype/front/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-import json
+import jsonq
 from django.views.decorators.csrf import csrf_exempt
 from . import lect_json
 
